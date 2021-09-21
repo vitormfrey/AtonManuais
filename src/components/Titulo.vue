4faca23c24e870f64cac6f17169decba6c2af1da
Vitor Moisés de Freitas Rey <vitor@atonsystems.com.br>
2021-09-20 21:11:02 -0300

    [Componente] Criação componente Titulo

---
+++ src/components/Titulo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="titulo">AtonSystems Manuais</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Titulo',
+  props: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.titulo {
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 59px;
+  text-transform: capitalize;
+
+  color: #003561;
+}
+</style>
