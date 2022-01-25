e2b7b173b97115627b213c16dd589bce8cee5fdc
Vitormfrey <vitormoises.rey@gmail.com>
2022-01-25 10:37:53 -0300

    [AXIOS] settings env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
+if (process.env.NODE_ENV == 'production') {
+  console.log('producao')
+} else {
+  console.log('dev')
+  console.log(process.env)
+}
+
 export default axios.create({
-  baseURL: process.env.VUE_APP_ROOT_API
+  baseURL: process.env.VUE_APP_API_URL
 })
