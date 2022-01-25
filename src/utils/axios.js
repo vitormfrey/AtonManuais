5da5c825b17f911f167f12c0a30b19414e23cbac
Vitormfrey <vitormoises.rey@gmail.com>
2022-01-25 11:54:02 -0300

    Revert "test env"
    This reverts commit 3313561145fb619a144e411ecacc268039ba0813.
    
    Revert "teste"
    
    This reverts commit dff7c3a7829645e47103a7471c9583bc77967d46.
    
    Revert "[AXIOS] test env"
    
    This reverts commit e567b75424d328b5fdc40c7527094c38774914bd.
    
    Revert "[AXIOS] settings env"
    
    This reverts commit e2b7b173b97115627b213c16dd589bce8cee5fdc.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,15 +1,5 @@
 import axios from 'axios'
 
-if (process.env.NODE_ENV == 'production') {
-  console.log(process.env)
-  console.log(process.env.VUE_APP_VERCEL_URL)
-  console.log('producao')
-} else {
-  console.log('dev')
-  console.log(process.env.VUE_APP_VERCEL_URL)
-  console.log(process.env)
-}
-
 export default axios.create({
-  baseURL: process.env.VUE_APP_VERCEL_URL
+  baseURL: process.env.VUE_APP_ROOT_API
 })
