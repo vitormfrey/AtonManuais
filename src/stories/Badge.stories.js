7d1a6fe71d7824be30ed90b9a0931c6a2f0ea5bb
vitormfrey <vitormoises.rey@gmail.com>
2022-07-20 13:58:34 -0300

    [dependency] Storybook

---
+++ src/stories/Badge.stories.js
@@ -0,0 +1,9 @@
+import Badge from '../components/Badge/index.vue'
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Badge',
+  component: Badge
+}
